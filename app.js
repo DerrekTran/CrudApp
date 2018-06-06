362d6ed3d10f90ba7a5600a7e8a6c894b500b859
DERREK TRAN <blubby@derreks-mbp.attlocal.net>
2018-06-05 21:27:05 -0700

    able to store and display all snippets

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 $(document).ready(function(){
-	$('.setData').on('click', function() {
-		let textFieldValue = $('.textField').val();
-		$('.debug').text(textFieldValue);
 
-		localStorage.setItem('myFormTextData', textFieldValue);
-	});
+  $('.setData').on('click', function() {
+    let textFieldValue = $('.textField').val();
+    let snippetValue = $('.snippetField').val();
+    localStorage.setItem(JSON.stringify(textFieldValue), JSON.stringify(snippetValue));
+    // $('.textField').val('');
+    // $('.snippetField').val('')
+   // 	$('.debug').text(textFieldValue);
+    //	$('.debug').text(snippetValue);
+  });
 
-	$('.getData').on('click', function() {
-		let retrievedData = localStorage.getItem('myFormTextData');
-		$('.debug').text(retrievedData);
-	});	
-	// $('.textField').on('keyup', function(){
-	// 	let textFieldValue = $('.textField').val();
-	// 	$('.debug').text(textFieldValue);
-	// });
+  // const snippetArray = [];
+  $('.getData').on('click', function(){
+ //   let retrievedData = localStorage.getItem('myFormTextData');
+ 	 // $('.snippetDisplay').remove();
+ 	 const snippetArray = [];
+ 	 
+    for (var i = 0; i < localStorage.length; i++) {
+    	if(localStorage.key(i) && localStorage.getItem(localStorage.key(i))) {
+    		console.log(localStorage.key(i) + ':' + localStorage.getItem(localStorage.key(i)));
+    		snippetArray.push(JSON.parse(localStorage.key(i)) + ':' + JSON.parse(localStorage.getItem(localStorage.key(i))));
+    		
+    	}
+
+ //   	retrievedData.appendTo(snippetDisplay)
+    }
+    $('.snippetDisplay').replaceWith($('<div class="snippetDisplay">' + snippetArray + '</div>'))
+
+ //   $('.debug').text(retrievedData);
+  });
 
 });
